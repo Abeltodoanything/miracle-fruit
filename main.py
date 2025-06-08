f26d8eb6dcd40201a87ff592d350dbfed2e74530
abe <149753452+Abeltodoanything@users.noreply.github.com>
2025-06-07 20:23:41 -0700

    Update

---
+++ main.py
@@ -0,0 +1,67 @@
+import tkinter as tk
+import ttkbootstrap as ttk
+import customtkinter as ctk
+
+# Main window
+window = ttk.Window(themename='darkly')
+window.attributes('-topmost', True)
+window.title("Auto Clicker")
+window_width = 500
+window_height = 600
+comp_width = window.winfo_screenwidth()
+comp_height = window.winfo_screenheight()
+
+left = int(comp_width /2 - window_width /2)
+top = int(comp_height /2 - window_height /2)
+
+window.geometry(f'{window_width}x{window_height}+{left}+{top}')
+
+# Click interval FRAME
+click_int = ttk.Labelframe(window, text='Click interval')
+click_int.pack()
+
+hours_entry = ttk.Entry(click_int, width=7, justify='right')
+hours_entry.grid(column=0, row=0, padx=4,pady=4)
+hours_label = ttk.Label(click_int, text='hours')
+hours_label.grid(column=1, row=0, padx=4,pady=4)
+
+mins_entry = ttk.Entry(click_int, width=7, justify='right')
+mins_entry.grid(column=2, row=0, padx=4,pady=4)
+mins_label = ttk.Label(click_int, text='mins')
+mins_label.grid(column=3, row=0, padx=4,pady=4)
+
+secs_entry = ttk.Entry(click_int, width=7, justify='right') 
+secs_entry.grid(column=4, row=0, padx=4,pady=4)
+secs_label = ttk.Label(click_int, text='secs')
+secs_label.grid(column=5, row=0, padx=4,pady=4)
+
+miliseconds_entry = ttk.Entry(click_int, width=7, justify='right')
+miliseconds_entry.grid(column=6, row=0, padx=4,pady=4)
+miliseconds_label = ttk.Label(click_int, text='miliseconds')
+miliseconds_label.grid(column=7, row=0, padx=4,pady=4)
+
+# Frame
+opt_rep = ttk.Frame(window)
+# Click options FRAME
+click_opt = ttk.Labelframe(opt_rep, text='Click options')
+click_opt.grid(fill='x', anchor='nw')
+ttk.Button(click_opt, text='button').pack()
+
+# Click repeat FRAME
+click_rep = ttk.Labelframe(opt_rep, text='Click repeat')
+click_rep.grid(fill='x', anchor='ne')
+ttk.Button(click_rep, text='button').pack()
+
+# Cursor position FRAME
+cursor_pos = ttk.Labelframe(window, text='Text position')
+cursor_pos.pack()
+ttk.Button(cursor_pos, text='button').pack()
+
+# Start/Stop | HotKey setting | Record & Playback
+
+start_frame = ttk.Frame()
+
+
+
+# run
+window.mainloop()
